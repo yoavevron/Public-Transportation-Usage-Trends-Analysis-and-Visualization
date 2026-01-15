8e87fce98d828634c62c457fab335e88edef2c8e
yoavevron <yoavev2020@gmail.com>
2026-01-15 19:22:30 +0200

    added small data 5->15

--- cut_data.py
+++ cut_data.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-df = pd.read_parquet('full_data.parquet')
+df = pd.read_parquet("data_full.parquet")
 
 city_grouped = (
     df.groupby("CityName", as_index=False)
       .agg(total_rides=("total_rides", "sum"))
+      .sort_values("total_rides", ascending=False)
+      .reset_index(drop=True)
 )
 
-top_10_cities = city_grouped.nlargest(10, "total_rides")["CityName"]
-df_small = df[df["CityName"].isin(top_10_cities)].copy()
-df_small.to_parquet("data.parquet", index=False)
+# Select ranks 5 to 15
+selected_cities = city_grouped.iloc[4:15]["CityName"]
+
+df_small = df[df["CityName"].isin(selected_cities)].copy()
+
+df_small.to_parquet("data_small.parquet", index=False)
